7f95d77bc1309ac1ec65b66668ca13365f95c7f1
SungWook Choi <choisw950928@naver.com>
2022-01-29 23:45:47 +0900

    <CarRacingGame(시작)> 22.01.29

--- src/car/racing/game/Input.java
+++ src/car/racing/game/Input.java
@@ -6,16 +6,9 @@ public class Input {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public String[] enterCarName() { // 자동차 이름 입력
-        participateCarNameMessage();
-
-        String[] carName = SCANNER.nextLine().split(",");
-
-        return carName;
-    }
-
-    public static void participateCarNameMessage() {
+    public String inputCarName() { // 자동차 이름 입력
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return SCANNER.next();
     }
 
 }
